5fa06b85c58bfd8ccc2d585fda93cec2a5733204
Rickzint5 <paulohnrique987@gmail.com>
2026-04-08 19:11:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+var client = new DiscordSocketClient(new DiscordSocketConfig
+{
+    GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages
+});
+
+var services = new ServiceCollection()
+    .AddSingleton(client)
+    .BuildServiceProvider();
+
+var interactionService = new InteractionService(client);
+
+client.Log += msg => { Console.WriteLine(msg); return Task.CompletedTask; };
+client.Ready += async () =>
+{
+    await interactionService.AddModuleAsync<NukeModule>(services);
+    await interactionService.RegisterCommandsGloballyAsync();
+    Console.WriteLine($"Bot online como {client.CurrentUser.Username}");
+};
+client.InteractionCreated += async interaction =>
+{
+    var ctx = new SocketInteractionContext(client, interaction);
+    await interactionService.ExecuteCommandAsync(ctx, services);
+};
+
+var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")
+    ?? throw new Exception("DISCORD_TOKEN não configurado!");
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+await Task.Delay(Timeout.Infinite);
+
+public class NukeModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("nuke", "Limpa todas as mensagens do canal")]
+    [RequireUserPermission(GuildPermission.ManageChannels)]
+    public async Task NukeAsync()
+    {
+        await DeferAsync(ephemeral: true);
+
+        var channel = (ITextChannel)Context.Channel;
+        var newChannel = await channel.Guild.CreateTextChannelAsync(channel.Name, props =>
+        {
+            props.Topic = channel.Topic;
+            props.CategoryId = channel.CategoryId;
+            props.Position = channel.Position;
+            props.IsNsfw = channel.IsNsfw;
+            props.SlowModeInterval = channel.SlowModeInterval;
+            props.PermissionOverwrites = new Optional<IEnumerable<Overwrite>>(
+                channel.PermissionOverwrites.ToList()
+            );
+        });
+
+        await channel.DeleteAsync();
+
+        var embed = new EmbedBuilder()
+            .WithDescription($"'canal nukado por {Context.User.Username}'")
+            .WithColor(new Discord.Color(0x2B2D31))
+            .Build();
+
+        await newChannel.SendMessageAsync(embed: embed);
+    }
+}
