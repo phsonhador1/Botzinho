a919d4b20b0d78fc2caf4b96ea6289b2cc3d804f
Rickzint5 <paulohnrique987@gmail.com>
2026-04-21 17:20:03 -0300

    Update CassinoModule.cs

--- CassinoModule.cs
+++ CassinoModule.cs
@@ -311,7 +311,7 @@ namespace Botzinho.Cassino
                     .WithThumbnailUrl("https://media.discordapp.net/attachments/1168256801981079632/1168536905399406592/gambling_5458667.png?ex=69e8c84c&is=69e776cc&hm=acd4a36afc7040e01b3a7d5716b60e2c96e2938bf9e7db0fcdd111661a661508&")
                     .WithDescription($@"<a:teste:1490570407307378712> **Olá, {user.Mention}! Bem-vindo(a) à Roleta da {_client.CurrentUser.Username}.**
 
-<:moedazoe:1493359715420340364>| **Valor em aposta:** `{EconomyHelper.FormatarSaldo(valorAposta)}`
+<:moedazoe:1493359715420340364> | **Valor em aposta:** `{EconomyHelper.FormatarSaldo(valorAposta)}`
 
 <:seta:1493089125979656385> | **Como funciona:** Ao escolher uma cor abaixo, representada pelos botões, você terá a chance de 
 ganhar com base nos multiplicadores. Cada cor tem 
